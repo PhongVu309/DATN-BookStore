dea9618acc042737cf251cbb1cd1d5d622b56daf
PhongVu309 <phongvu3092003@gmail.com>
2025-03-04 15:27:51 +0700

    cata

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LayoutHeaderComponent } from "./component/layout-header/layout-header.component";
+import { LayoutFooterComponent } from "./component/layout-footer/layout-footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LayoutHeaderComponent],
+  imports: [RouterOutlet, LayoutHeaderComponent, LayoutFooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
