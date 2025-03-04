0c1fe68319a976b33ef42f6218a2992bc438fb35
PhongVu309 <phongvu3092003@gmail.com>
2025-03-05 00:08:41 +0700

    home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './component/home/home.component';
+import { LoginComponent } from './component/auth/login/login.component';
+import { RegisterComponent } from './component/auth/register/register.component';
 
 export const routes: Routes = [
   {path: '', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path:'register', component: RegisterComponent}
 ];
