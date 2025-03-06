715978c313910038f631fdc8a6377b0ad7093033
PhongVu309 <phongvu3092003@gmail.com>
2025-03-06 23:54:25 +0700

    info products

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './component/home/home.component';
 import { LoginComponent } from './component/auth/login/login.component';
 import { RegisterComponent } from './component/auth/register/register.component';
+import { DetailProductsComponent } from './component/detail-products/detail-products.component';
 
 export const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'login', component: LoginComponent},
-  {path:'register', component: RegisterComponent}
+  {path:'register', component: RegisterComponent},
+  {path: 'detail-product', component: DetailProductsComponent},
 ];
