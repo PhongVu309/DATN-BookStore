29c47065d50475cd06aba33192f0b4a2a475999d
PhongVu309 <phongvu3092003@gmail.com>
2025-03-07 15:11:13 +0700

    l

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -1,12 +1,59 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  products = [
+    {
+      name: 'Xin Chào, Nỗi Sợ (Hello, Fears)',
+      price: 72000,
+      originalPrice: 120000,
+      discount: 40,
+      imageUrl: 'assets/img/home/flashsale/xin-chao-noi-so-1.jpg',
+      rating: 5,
+      reviews: 79
+    },
+    {
+      name: 'Đừng Thách Tôi',
+      price: 72000,
+      originalPrice: 120000,
+      discount: 40,
+      imageUrl: 'assets/img/home/flashsale/dung-thach-toi-1.jpg',
+      rating: 5,
+      reviews: 79
+    },
+    {
+      name: 'Tỏa Sáng (Shine)',
+      price: 72000,
+      originalPrice: 120000,
+      discount: 40,
+      imageUrl: 'assets/img/home/flashsale/toa-sang-shine.jpg',
+      rating: 5,
+      reviews: 79
+    },
+    {
+      name: 'Tỏa Sáng (Shine)',
+      price: 72000,
+      originalPrice: 120000,
+      discount: 40,
+      imageUrl: 'assets/img/home/flashsale/toa-sang-shine.jpg',
+      rating: 5,
+      reviews: 79
+    },
+    {
+      name: 'Tỏa Sáng (Shine)',
+      price: 72000,
+      originalPrice: 120000,
+      discount: 40,
+      imageUrl: 'assets/img/home/flashsale/toa-sang-shine.jpg',
+      rating: 5,
+      reviews: 79
+    }
+  ];
 }
